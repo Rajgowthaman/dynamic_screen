7f37ab541b8a3b0d4a2ab0d5ac49800cafb5e3b8
rajgowthaman <rajgowthaman94@gmail.com>
2020-08-03 21:55:53 +0530

    console app starting message

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8000
 const dynamicScreenContr = require('./controller/dynamicScreenController');
 
 //Controllers
 app.use('/', dynamicScreenContr);
 //End Controllers
-app.listen(process.env.PORT || 8000);
+app.listen(port, ()=>{
+    console.log("App listening on the port : "+port);
+});
